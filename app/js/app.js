99c71e824e2f554d05dd7ba6f867eefe560bd83f
Shyam Seshadri <shyamseshadri@gmail.com>
2013-03-12 13:31:35 +0530

    Session 1 - Step 2: Use ng repeat and add ng-class

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,9 @@ var angularApp = angular.module('RecipeApp', []);
 
 angularApp.controller('RecipeListCtrl', ['$scope', function($scope) {
   $scope.recipes = [
-    {id: 1, author: 'Shyam Seshadri', name: 'Recipe 1', imgSrc: ''},
-    {id: 2, author: 'Shyam Seshadri', name: 'Recipe 2', imgSrc: ''},
-    {id: 3, author: 'Shyam Seshadri', name: 'Recipe 3', imgSrc: ''},
-    {id: 4, author: 'Shyam Seshadri', name: 'Recipe 4', imgSrc: ''}
+    {id: 1, author: 'Shyam Seshadri', name: 'Paneer Tikka Masala', imgSrc: ''},
+    {id: 2, author: 'Shyam Seshadri', name: 'Goan Fish Curry', imgSrc: '', featured: true},
+    {id: 3, author: 'Someone else', name: 'Tandoori Naan', imgSrc: ''},
+    {id: 4, author: 'Someone else', name: 'Paneer lababdar', imgSrc: '', featured: true}
   ];
 }]);
