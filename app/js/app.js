1028c089e037794727b6f08179030b404be50518
Shyam Seshadri <shyamseshadri@gmail.com>
2013-03-15 21:31:24 +0530

    Session 5 - Step 4: Add working date picker widget

--- app/js/app.js
+++ app/js/app.js
@@ -25,6 +25,51 @@ angularApp.directive('recipePoster', function() {
   };
 });
 
+angularApp.directive('datepicker', function() {
+  return {
+    // Enforce the angularJS default of restricting the directive to
+    // attributes only
+    restrict: 'A',
+    // Always use along with an ng-model
+    require: '?ngModel',
+    // This method needs to be defined and passed in from the
+    // passed in to the directive from the view controller
+    scope: {
+      select: '&'        // Bind the select function we refer to the right scope
+    },
+    link: function(scope, element, attrs, ngModel) {
+      if (!ngModel) return;
+
+      var optionsObj = {};
+
+      optionsObj.dateFormat = 'mm/dd/yy';
+      var updateModel = function(dateTxt) {
+        scope.$apply(function () {
+          // Call the internal AngularJS helper to
+          // update the two way binding
+          ngModel.$setViewValue(dateTxt);
+        });
+      };
+
+      optionsObj.onSelect = function(dateTxt, picker) {
+        updateModel(dateTxt);
+        if (scope.select) {
+          scope.$apply(function() {
+            scope.select({date: dateTxt});
+          });
+        }
+      };
+
+      ngModel.$render = function() {
+        // Use the AngularJS internal 'binding-specific' variable
+        element.datepicker('setDate', ngModel.$viewValue || '');
+      };
+      element.datepicker(optionsObj);
+    }
+  };
+  });
+
+
 angularApp.factory('Recipe', ['$resource', function($resource) {
   return $resource('/api/recipe/:id', {id: '@id'});
 }]);
